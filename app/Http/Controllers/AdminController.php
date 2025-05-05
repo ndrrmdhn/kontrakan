b1ba03ed514c47a918d315892e0070015b92a6bf
ndrrmdhn <andreramadhani98@gmail.com>
2025-05-05 12:33:03 +0700

    Admin Profile Change Password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -37,7 +38,7 @@ class AdminController extends Controller
         $data = User::find($id);
         $data->username = $request->username;
         $data->name = $request->name;
-        $data->email = $request->email;
+        // $data->email = $request->email;
         $data->phone = $request->phone;
         $data->address = $request->address;
         
@@ -55,4 +56,77 @@ class AdminController extends Controller
         );
         return redirect()->route('admin.profile')->with($notification);
     }
+
+    public function AdminChangePassword(){
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin-change-password', compact('profileData'));
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        // Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed|min:8',
+        ]);
+
+        // Check if the old password is correct
+        if (!Hash::check($request->old_password, Auth::user()->password)) {
+            return back()->with([
+                'message' => 'Old Password is Incorrect',
+                'alert-type' => 'error'
+            ]);
+        }
+
+        // Check if the new password is the same as the old password
+        if ($request->old_password === $request->new_password) {
+            return back()->with([
+                'message' => 'New Password must be different from the Old Password',
+                'alert-type' => 'warning'
+            ]);
+        }
+
+        // Update the password
+        $user = User::find(Auth::id());
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return back()->with([
+            'message' => 'Password Updated Successfully',
+            'alert-type' => 'success'
+        ]);
+    }
+
+    // public function AdminUpdatePassword(Request $request){
+        
+    //     // Validation
+    //     $request->validate([
+    //         'old_password' => 'required',
+    //         'new_password' => 'required|confirmed|min:8',
+    //     ]);
+
+    //     // Check if the old password is correct
+    //     if (!Hash::check($request->old_password, Auth::user()->password)) {
+    //         $notification = array(
+    //             'message' => 'Old Password is Incorrect',
+    //             'alert-type' => 'error'
+    //         );
+    //         return back()->with($notification);
+
+    //     // Update the password
+    //     // User::where('id', Auth::user()->id)->update([
+    //     //     'password' => Hash::make($request->new_password)
+    //     // ]);
+    //     User::whereId(Auth::user()->id)->update([
+    //         'password' => Hash::make($request->new_password)
+    //     ]);
+    //         $notification = array(
+    //             'message' => 'Password Updated Successfully',
+    //             'alert-type' => 'success'
+    //         );
+    //         return back()->with($notification);
+    //     }
+    // }
+
 }
--- resources/views/admin/admin-profile.blade.php
+++ resources/views/admin/admin-profile.blade.php
@@ -75,14 +75,14 @@
                                           <label for="exampleInputName1" class="form-label">Address</label>
                                           <input type="text" name="address" class="form-control" id="exampleInputName1" autocomplete="off" value="{{ $profileData->address }}">
                                       </div>
-                                      <div class="mb-3">
+                                      {{-- <div class="mb-3">
                                           <label for="exampleInputEmail1" class="form-label">Email</label>
                                           <input type="email" name="email" class="form-control" id="exampleInputEmail1" value="{{ $profileData->email }}">
                                       </div>
                                       <div class="mb-3">
                                           <label for="exampleInputPassword1" class="form-label">Password</label>
                                           <input type="password" name="password" class="form-control" id="exampleInputPassword1" autocomplete="off" value="{{ $profileData->password }}">
-                                      </div>
+                                      </div> --}}
                                       <div class="mb-3">
                                           <label for="exampleInputPhoto1" class="form-label">Photo</label>
                                           <input class="form-control" type="file" id="image" name="photo">
